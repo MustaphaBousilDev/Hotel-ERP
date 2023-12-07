3dc156567edc9cb94c00ce2b16dd24399e4cbe5c
mustaphabousildev <bousilmustapha@gmail.com>
2023-12-07 22:27:39 +0100

    create configuration module and connect to database mongodb

---
+++ servers/libs/shared/src/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService], // Specify the dependencies that should be injected into the useFactory function
+    }),
+  ],
+})
+export class DatabaseModule {}
--- servers/src/main.ts
+++ servers/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('fucking nest')
+  console.log('fucking nest');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
