917e6efecd274370a03fec61e17cbdd5c9c6be6b
mustaphabousildev <bousilmustapha@gmail.com>
2023-12-25 17:42:58 +0100

    finish tabs design and logic in component angular

--- clients/tasks/src/app/components/tabs/tabs.component.ts
+++ clients/tasks/src/app/components/tabs/tabs.component.ts
@@ -10,10 +10,28 @@ import {MatTabsModule} from '@angular/material/tabs';
 export class TabsComponent {
   title = 'Bytes';
   activeTab: string = 'all-task';
+  type:string | null = '';
+
+  ngOnInit() {
+    // Call your custom function when the component is initialized
+    this.type=localStorage.getItem('type')
+    if(this.type){
+      this.activeTab = this.type
+      // console.log('init',this.activeTab)
+    }else{
+      localStorage.setItem('type',this.activeTab)
+    }
+  }
 
   onTabClick(tab:any) {
     this.activeTab = tab;
-    console.log(this.activeTab)
+    localStorage.setItem('type',this.activeTab)
+    // console.log(this.activeTab)
+  }
+
+  storeLocalStorage(data:string){
+    
+    
   }
 }
 
--- docker.js
+++ docker.js
@@ -5,4 +5,5 @@
  * #03-- (-f Dockerfile) specify the name of the Dockerfileto use for the build,By default, Docker looks for a file named "Dockerfile" in the build context.However, if your Dockerfile has a different name or is located in a different directory, you use the -f option followed by the path to the Dockerfile. 
  * #04-- (-t sleeper_reservation) This option is used to tag the resulting Docker image. 
  * #05--  -t:option allows you to associate a name and optionally a tag with the Docker image. In this case, the image will be tagged with the name "sleeper_reservation".
+ * (root: /reservation --> docker run sleeper_reservation) for run image sleeper_reservation
  */
