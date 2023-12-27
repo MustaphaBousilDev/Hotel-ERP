6fce8269ed06748a0a28ea15a9528b04a374bf97
mustaphabousildev <bousilmustapha@gmail.com>
2023-12-27 20:31:04 +0100

    create JWT Strategy

--- servers/apps/auth/src/users/users.service.ts
+++ servers/apps/auth/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcryptjs';
+import { GetUserDto } from './dto/get-user.dto';
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
@@ -24,5 +25,9 @@ export class UsersService {
     console.log('-----------------------------------------------------\n');
     //user.password = 'bitch local strategy';
     return user;
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    return this.usersRepository.findOne(getUserDto);
   }
 }
