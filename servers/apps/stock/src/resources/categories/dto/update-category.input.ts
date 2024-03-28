617cb47ff4be42cd010b10e90b26aac0d76c7cab
Mustapha Bousil <bousilmustapha@gmail.com>
2024-03-28 23:10:52 +0000

    finish fix error management Category in stock microservice

--- servers/apps/stock/src/resources/categories/dto/update-category.input.ts
+++ servers/apps/stock/src/resources/categories/dto/update-category.input.ts
@@ -3,18 +3,18 @@ import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class UpdateCategoryInput {
-  @Field()
+  @Field({ nullable: true })
   @IsOptional()
   @IsString()
   name?: string;
 
-  @Field()
+  @Field({ nullable: true })
   @IsOptional()
   @IsBoolean()
   status?: boolean;
 
   @IsOptional()
-  @Field()
+  @Field({ nullable: true })
   @IsString()
   image?: string;
 }
