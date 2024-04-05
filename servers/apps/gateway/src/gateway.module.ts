a2838d281ddfae19d12161e638c3fd2f9714cbfb
Mustapha Bousil <bousilmustapha@gmail.com>
2024-04-05 01:38:58 +0000

    finish

--- servers/apps/gateway/src/gateway.module.ts
+++ servers/apps/gateway/src/gateway.module.ts
@@ -19,34 +19,34 @@ import { authContext } from './auth.context';
         gateway: {
           supergraphSdl: new IntrospectAndCompose({
             subgraphs: [
-              /*{
+              {
                 name: 'reservations',
                 url: configService.getOrThrow('RESERVATIONS_GRAPHQL_URL'),
-              },*/
+              },
               {
                 name: 'auth',
                 url: configService.getOrThrow('AUTH_GRAPHQL_URL'),
               },
-              /*{
+              {
                 name: 'payments',
                 url: configService.getOrThrow('PAYMENTS_GRAPHQL_URL'),
-              },*/
-              /*{
+              },
+              {
                 name: 'organization',
                 url: configService.getOrThrow('ORGANIZATION_GRAPHQL_URL'),
               },
               {
                 name: 'tasks',
                 url: configService.getOrThrow('TASKS_GRAPHQL_URL'),
-              },*/
+              },
               {
                 name: 'employee',
                 url: configService.getOrThrow('EMPLOYEE_GRAPHQL_URL'),
               },
-              /*{
+              {
                 name: 'stock',
                 url: configService.getOrThrow('STOCK_GRAPHQL_URL'),
-              },*/
+              },
             ],
           }),
           buildService({ url }) {
